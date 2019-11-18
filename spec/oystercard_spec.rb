557538023e07593c9aecd5ce58f4ab42a5ae1728
dbacall <dbacall@hotmail.co.uk>
2019-11-18 15:03:21 +0000

    add top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,10 +2,18 @@ require "oystercard"
 
 describe Oystercard do
 
+  let(:oystercard) { Oystercard.new }
+
   describe "#balance" do
     it "returns 0 for a new card" do
-      oystercard = Oystercard.new
       expect(oystercard.balance).to eq 0
+    end
+  end
+
+  describe "#top_up" do
+    it "adds 20 to balance" do
+      oystercard.top_up(20)
+      expect(oystercard.balance).to eq 20
     end
   end
 
