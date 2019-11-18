37bd73ca45ca11e1ef79a960a4ae5c958a3f58e6
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2019-11-18 14:47:06 +0000

    Add test and code for Oystercard instance variable 'balance'

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,11 @@ require "oystercard"
 
 describe Oystercard do
 
+  describe "#balance" do
+    it "returns 0 for a new card" do
+      oystercard = Oystercard.new
+      expect(oystercard.balance).to eq 0
+    end
+  end
+
 end
